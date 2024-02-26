91dd5d39c26a7a1b0045a22c239a3646d6be769b
ThatStevenGuy <65724470+ThatStevenGuy@users.noreply.github.com>
2024-02-26 20:39:31 +0100

    The Neovim config in ~/.config/nvim is now mounted to the container.

--- mount/post_launch/neovim_config.sh
+++ mount/post_launch/neovim_config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-  git clone git@github.com:ThatStevenGuy/nvimrc.git "$HOME/.config/nvim"
-fi
-
 if ! grep -q "alias nvimc=" "$HOME/.bashrc"; then
   echo 'alias nvimc="cd ~/.config/nvim && nvim ."' >> "$HOME/.bashrc"
 fi
